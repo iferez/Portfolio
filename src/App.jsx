3de51b1696460029c19264695ff6cfb8daba9498
Ivan Ferez <ferezivan@hotmail.com>
2023-02-25 20:38:31 -0300

    Inicio de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default App;
